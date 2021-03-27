b6748cef5515ed5767faa82305506f838eb1240d
Hunter King <hking0036@gmail.com>
2021-03-27 00:23:36 -0400

    add barcodescanner to package.json

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
 export default function App() {
   return (
